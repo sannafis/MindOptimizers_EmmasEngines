3006f6cb974c96ea0b79b19ad33ee07c5e3c9c7f
JCMilleniuM <jcmoreno@w3b.ec>
2023-02-05 15:00:58 -0500

    Customers page created

---
+++ EmmasEngines/Controllers/CustomersController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmmasEngines.Data;
+using EmmasEngines.Models;
+using EmmasEngines.Utilities;
+
+namespace EmmasEngines.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly EmmasEnginesContext _context;
+
+        public CustomersController(EmmasEnginesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index(string SearchString, string actionButton, int? page, int? pageSizeID, string sortDirection = "asc", string sortField = "Name")
+        {
+            //List of sort options
+            string[] sortOptions = new[] { "ID", "FName", "LName" };
+            
+            
+            var emmasEnginesContext = _context.Customers
+                .Include(c => c.City)
+                .AsNoTracking();
+            
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                emmasEnginesContext = emmasEnginesContext.Where(
+                    s => s.FirstName.ToLower().Contains(SearchString.ToLower()) ||
+                         s.LastName.ToLower().Contains(SearchString.ToLower()) ||
+                         s.City.Name.ToLower().Contains(SearchString.ToLower())
+                );
+            }
+            
+            return View(await emmasEnginesContext.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.City)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            ViewData["CityID"] = new SelectList(_context.Cities, "ID", "Name");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,LastName,Phone,Address,Postal,CityID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityID"] = new SelectList(_context.Cities, "ID", "Name", customer.CityID);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityID"] = new SelectList(_context.Cities, "ID", "Name", customer.CityID);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,Phone,Address,Postal,CityID")] Customer customer)
+        {
+            if (id != customer.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityID"] = new SelectList(_context.Cities, "ID", "Name", customer.CityID);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.City)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'EmmasEnginesContext.Customers'  is null.");
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ EmmasEngines/Data/EmmasEnginesMigrations/20230205191239_Initial.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmmasEngines.Data.EmmasEnginesMigrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UPC = table.Column<string>(type: "TEXT", maxLength: 11, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Size = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Quantity = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Current = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.ID);
+                    table.UniqueConstraint("AK_Inventories_UPC", x => x.UPC);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provinces",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Code = table.Column<string>(type: "TEXT", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provinces", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    ProvinceCode = table.Column<string>(type: "TEXT", nullable: false),
+                    ProvinceID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Cities_Provinces_ProvinceID",
+                        column: x => x.ProvinceID,
+                        principalTable: "Provinces",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 12, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Postal = table.Column<string>(type: "TEXT", maxLength: 7, nullable: false),
+                    ProvinceID = table.Column<int>(type: "INTEGER", nullable: true),
+                    CityID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Customers_Cities_CityID",
+                        column: x => x.CityID,
+                        principalTable: "Cities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Customers_Provinces_ProvinceID",
+                        column: x => x.ProvinceID,
+                        principalTable: "Provinces",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 12, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Postal = table.Column<string>(type: "TEXT", maxLength: 7, nullable: false),
+                    ProvinceID = table.Column<int>(type: "INTEGER", nullable: true),
+                    CityID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Suppliers_Cities_CityID",
+                        column: x => x.CityID,
+                        principalTable: "Cities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Suppliers_Provinces_ProvinceID",
+                        column: x => x.ProvinceID,
+                        principalTable: "Provinces",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prices",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    InventoryUPC = table.Column<string>(type: "TEXT", nullable: false),
+                    PurchasedCost = table.Column<double>(type: "REAL", nullable: false),
+                    PurchasedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Stock = table.Column<int>(type: "INTEGER", nullable: false),
+                    SupplierID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prices", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Prices_Inventories_InventoryUPC",
+                        column: x => x.InventoryUPC,
+                        principalTable: "Inventories",
+                        principalColumn: "UPC",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Prices_Suppliers_SupplierID",
+                        column: x => x.SupplierID,
+                        principalTable: "Suppliers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_ProvinceID",
+                table: "Cities",
+                column: "ProvinceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CityID",
+                table: "Customers",
+                column: "CityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_ProvinceID",
+                table: "Customers",
+                column: "ProvinceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_UPC",
+                table: "Inventories",
+                column: "UPC",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prices_InventoryUPC",
+                table: "Prices",
+                column: "InventoryUPC");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prices_SupplierID",
+                table: "Prices",
+                column: "SupplierID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provinces_Code",
+                table: "Provinces",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provinces_Name",
+                table: "Provinces",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_CityID",
+                table: "Suppliers",
+                column: "CityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_ProvinceID",
+                table: "Suppliers",
+                column: "ProvinceID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Prices");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Provinces");
+        }
+    }
+}
