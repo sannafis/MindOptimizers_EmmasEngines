32a61e7798a1d72d3db3b5e99e13bf1b440d79a3
Miguel Andres Sanchez Barbosa <msanchezbarbosa1@ncstudents.niagaracollege.ca>
2023-03-07 15:48:24 -0500

    fix nav bar li padding fix pagination button size

--- EmmasEngines/wwwroot/js/site.js
+++ EmmasEngines/wwwroot/js/site.js
@@ -3,21 +3,3 @@
 
 // Write your JavaScript code.
 
-const collapsedClass = "nav--collapsed";
-const lskey = "navCollapsed";
-const tooltipClass = "active";
-
-const nav = document.querySelector(".home_header");
-const navBorder = nav.querySelector(".nav__border");
-//const menu = nav.querySelector(".menu");
-    
-if (localStorage.getItem(lskey) === "true") {
-    nav.classList.add(collapsedClass);
-    nav.classList.add(tooltipClass);
-}
-
-navBorder.addEventListener("click", () => {
-    nav.classList.toggle(collapsedClass);
-    nav.classList.toggle(tooltipClass);
-    localStorage.setItem(lskey, nav.classList.contains(collapsedClass));
-});
