12a120b9d2332b058f4f3d18aced94a04128aa74
JCMilleniuM <jcmoreno@w3b.ec>
2023-03-12 22:28:00 -0400

    remake OrderREquest based on new model

--- EmmasEngines/Controllers/OrderRequestsController.cs
+++ EmmasEngines/Controllers/OrderRequestsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EmmasEngines.Data;
 using EmmasEngines.Models;
-using EmmasEngines.Utilities;
 
 namespace EmmasEngines.Controllers
 {
@@ -21,92 +20,10 @@ namespace EmmasEngines.Controllers
         }
 
         // GET: OrderRequests
-        // Add items related to field item from OrderRequestInventory
-        
-        
-        
-        public async Task<IActionResult> Index(string SearchString, string query, string actionButton, int? page, int? pageSizeID, string sortDirection = "asc", string sortField = "Name")
+        public async Task<IActionResult> Index()
         {
-
-            ViewData["Filtering"] = "";
-
-            //sort options for the table (match column headings)
-            string[] sortOptions = new[] { "ID", "Description", "SentDate", "ReceiveDate", "ExternalOrderNum"};
-
-            var emmasEnginesContext = from i in _context.OrderRequests
-                .Include(o => o.Customer)
-                //.Include(o => o.OrderRequestInventories)
-				//.Include(o => o.Inventory)
-                .AsNoTracking()
-                select i;
-
-            if (!String.IsNullOrEmpty(SearchString))
-            {
-                //Filter by UPC and Name
-                emmasEnginesContext = emmasEnginesContext.Where(s => s.Description.ToUpper().Contains(SearchString.ToUpper()));
-                ViewData["Filtering"] = "show";
-                ViewData["Search Inventory"] = SearchString;
-            }
-
-            if (!String.IsNullOrEmpty(actionButton))//check if form submitted
-            {
-                page = 1;//reset page to 1
-                if (sortOptions.Contains(actionButton))
-                {
-                    if (actionButton == sortField)//reverse order  same column
-                    {
-                        sortDirection = (sortDirection == "asc" ? "desc" : "asc");
-                    }
-                    //sort by button clicked
-                    sortField = actionButton;
-                }
-            }
-            //Now sort by field and direction
-            if (sortField == "ID")
-            {
-                if (sortDirection == "asc")
-                    emmasEnginesContext = emmasEnginesContext.OrderBy(s => s.ID);
-                else
-                    emmasEnginesContext = emmasEnginesContext.OrderByDescending(s => s.ID);
-            }
-            if (sortField == "Description")
-            {
-                if (sortDirection == "asc")
-                    emmasEnginesContext = emmasEnginesContext.OrderBy(s => s.Description);
-                else
-                    emmasEnginesContext = emmasEnginesContext.OrderByDescending(s => s.Description);
-            }
-            else if (sortField == "SentDate")
-            {
-                if (sortDirection == "asc")
-                    emmasEnginesContext = emmasEnginesContext.OrderBy(s => s.SentDate);
-                else
-                    emmasEnginesContext = emmasEnginesContext.OrderByDescending(s => s.SentDate);
-            }
-            else if (sortField == "ReceiveDate")
-            {
-                if (sortDirection == "asc")
-                    emmasEnginesContext = emmasEnginesContext.OrderBy(s => s.ReceiveDate);
-                else
-                    emmasEnginesContext = emmasEnginesContext.OrderByDescending(s => s.ReceiveDate);
-            }
-            else if (sortField == "ExternalOrderNum")
-            {
-                if (sortDirection == "asc")
-                    emmasEnginesContext = emmasEnginesContext.OrderBy(s => s.ExternalOrderNum);
-                else
-                    emmasEnginesContext = emmasEnginesContext.OrderByDescending(s => s.ExternalOrderNum);
-            }
-            //set sort for in ViewData
-            ViewData["sortField"] = sortField;
-            ViewData["sortDirection"] = sortDirection;
-
-            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, "Inventory");
-            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
-
-            var pagedData = await PaginatedList<OrderRequest>.CreateAsync(emmasEnginesContext.AsNoTracking(), page ?? 1, pageSize);
-
-            return View(pagedData);
+            var emmasEnginesContext = _context.OrderRequests.Include(o => o.Customer).Include(o => o.Supplier);
+            return View(await emmasEnginesContext.ToListAsync());
         }
 
         // GET: OrderRequests/Details/5
@@ -119,6 +36,7 @@ namespace EmmasEngines.Controllers
 
             var orderRequest = await _context.OrderRequests
                 .Include(o => o.Customer)
+                .Include(o => o.Supplier)
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (orderRequest == null)
             {
@@ -132,6 +50,7 @@ namespace EmmasEngines.Controllers
         public IActionResult Create()
         {
             ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "Address");
+            ViewData["SupplierID"] = new SelectList(_context.Suppliers, "ID", "Address");
             return View();
         }
 
@@ -140,7 +59,7 @@ namespace EmmasEngines.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Description,SentDate,ReceiveDate,ExternalOrderNum,CustomerID")] OrderRequest orderRequest)
+        public async Task<IActionResult> Create([Bind("ID,Description,SentDate,ReceiveDate,ExternalOrderNum,CustomerID,SupplierID")] OrderRequest orderRequest)
         {
             if (ModelState.IsValid)
             {
@@ -149,6 +68,7 @@ namespace EmmasEngines.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "Address", orderRequest.CustomerID);
+            ViewData["SupplierID"] = new SelectList(_context.Suppliers, "ID", "Address", orderRequest.SupplierID);
             return View(orderRequest);
         }
 
@@ -166,6 +86,7 @@ namespace EmmasEngines.Controllers
                 return NotFound();
             }
             ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "Address", orderRequest.CustomerID);
+            ViewData["SupplierID"] = new SelectList(_context.Suppliers, "ID", "Address", orderRequest.SupplierID);
             return View(orderRequest);
         }
 
@@ -174,7 +95,7 @@ namespace EmmasEngines.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,SentDate,ReceiveDate,ExternalOrderNum,CustomerID")] OrderRequest orderRequest)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,SentDate,ReceiveDate,ExternalOrderNum,CustomerID,SupplierID")] OrderRequest orderRequest)
         {
             if (id != orderRequest.ID)
             {
@@ -202,6 +123,7 @@ namespace EmmasEngines.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["CustomerID"] = new SelectList(_context.Customers, "ID", "Address", orderRequest.CustomerID);
+            ViewData["SupplierID"] = new SelectList(_context.Suppliers, "ID", "Address", orderRequest.SupplierID);
             return View(orderRequest);
         }
 
@@ -215,6 +137,7 @@ namespace EmmasEngines.Controllers
 
             var orderRequest = await _context.OrderRequests
                 .Include(o => o.Customer)
+                .Include(o => o.Supplier)
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (orderRequest == null)
             {
