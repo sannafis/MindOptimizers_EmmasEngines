d0c20eb7c3e9b053926628c9547ae4bc86e8b51b
Ker Montero <KMONTERO2@ncstudents.niagaracollege.ca>
2023-03-30 18:09:17 -0400

    forgot pass Update

--- EmmasEngines/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ EmmasEngines/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -16,6 +16,7 @@ using Microsoft.AspNetCore.WebUtilities;
 
 namespace EmmasEngines.Areas.Identity.Pages.Account
 {
+    [Authorize(Roles = "Admin")]
     public class ForgotPasswordModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
