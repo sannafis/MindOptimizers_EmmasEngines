a4a6e72677948839f4907a5f4db9b17c69a2195d
sakha <119695583+sannafis@users.noreply.github.com>
2023-04-02 22:34:55 -0400

    hourly report done

---
+++ EmmasEngines/ViewModels/HourlyReportDetailsVM.cs
@@ -0,0 +1,12 @@
+﻿using EmmasEngines.Models;
+
+namespace EmmasEngines.ViewModels
+{
+    public class HourlyReportDetailsVM
+    {
+        public HourlyReport HourlyReport { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
